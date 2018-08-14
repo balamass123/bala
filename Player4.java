4c4f0704eabf52cc96557e36fbcffff0e29fe3c6
balamass123 <41642701+balamass123@users.noreply.github.com>
2018-08-14 14:40:54 +0530

    Create Player4.java

---
+++ Player4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Player4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        System.out.print(n+".");
+        
+    }
+}
