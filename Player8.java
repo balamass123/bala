74f43c0723f39e7daa0df0ea6beb3ec6614ebee3
balamass123 <41642701+balamass123@users.noreply.github.com>
2018-08-16 17:27:16 +0530

    Create Player8.java

---
+++ Player8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Player8
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s1[]=s.split(" ");
+        String s2="",s3="";
+        for(int i=0;i<s1.length;i++)
+        {
+            s3=s1[i].substring(1);
+            s2=String.valueOf(s1[i].substring(0,1)).toUpperCase(); 
+            System.out.print(s2+s3+" ");
+        }
+    }
+}
+
